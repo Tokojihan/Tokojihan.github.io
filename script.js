9851787d3bffbed1240afd3d03efe15ff938ae9d
kaizhr <zidanramadhan950@gmail.com>
2024-11-20 21:57:10 +0700

    tes

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.tombol-beli').forEach(button => {
+    button.addEventListener('click', function() {
+        alert("Produk telah ditambahkan ke keranjang!");
+    });
+});
