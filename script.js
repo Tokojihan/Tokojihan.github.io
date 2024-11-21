1f52d577cb17ee80e570f6b0ed61f01d359c3534
kaizhr <zidanramadhan950@gmail.com>
2024-11-21 18:21:59 +0700

    v

--- script.js
+++
@@ -1,6 +0,0 @@
-// Contoh tombol CTA interaktif
-const ctaButton = document.querySelector('.cta-button');
-
-ctaButton.addEventListener('click', () => {
-    alert('Selamat Berbelanja di Toko Jihan!');
-});
