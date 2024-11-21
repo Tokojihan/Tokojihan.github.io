8f513a4ec6a6346b8d9344dd50743357f794e4bd
kaizhr <zidanramadhan950@gmail.com>
2024-11-21 10:07:33 +0700

    t

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.querySelectorAll('.tombol-beli').forEach(button => {
-    button.addEventListener('click', function() {
-        alert("Produk telah ditambahkan ke keranjang!");
-    });
+// Responsive menu toggle
+const nav = document.querySelector('header nav ul');
+document.querySelector('header').addEventListener('click', () => {
+    nav.classList.toggle('active');
 });
