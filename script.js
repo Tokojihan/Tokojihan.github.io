cd516d700b9eb3afe788f1eaa163ab1cfcd1fce8
kaizhr <zidanramadhan950@gmail.com>
2024-11-21 17:00:41 +0700

    v

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Sederhana untuk menangani klik pada tombol "Tambah ke Keranjang"
-const addToCartButtons = document.querySelectorAll('.product-item button');
+// Contoh tombol CTA interaktif
+const ctaButton = document.querySelector('.cta-button');
 
-addToCartButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Produk telah ditambahkan ke keranjang!');
-    });
+ctaButton.addEventListener('click', () => {
+    alert('Selamat Berbelanja di Toko Jihan!');
 });
